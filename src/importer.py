0a23125b308c70b1aded5fb62012abe1b0f1da67
Ulrich Stark <ulrich.stark@gmx.de>
2020-12-07 14:53:41 +0100

    add types to parameter instead of manual checks on runtime

--- src/importer.py
+++ src/importer.py
@@ -5,17 +5,14 @@ import chardet
 class Importer:
     __importedRows = []
 
-    def __detectFileEncoding(self, filePath):
+    def __detectFileEncoding(self, filePath: str):
         with open(filePath, "rb") as testFile:
             testFileContent = testFile.read()
             chardetAnswer = chardet.detect(testFileContent)
             return chardetAnswer["encoding"]
 
-    def importCSVFile(self, filePath, encoding=None):
-        if not isinstance(filePath, str):
-            raise TypeError("filePath should be a string")
-
-        if not isinstance(encoding, str):
+    def importCSVFile(self, filePath: str, encoding: str = None):
+        if str is None:
             encoding = self.__detectFileEncoding(filePath)
 
         with open(filePath, "r") as file:
@@ -24,10 +21,7 @@ class Importer:
             for row in reader:
                 self.__importedRows.append(row)
 
-    def export(self, outputFilePath, newline=""):
-        if not isinstance(outputFilePath, str):
-            raise TypeError("outputFilePath should be a string")
-
+    def export(self, outputFilePath: str, newline=""):
         with open(outputFilePath, "w", newline=newline) as outputFile:
             writer = csv.writer(outputFile)
             writer.writerows(self.__importedRows)
