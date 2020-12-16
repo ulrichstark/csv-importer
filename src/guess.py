395d891f7e2ce1cf1635234415b600a258fb8c9f
Ulrich Stark <ulrich.stark@gmx.de>
2020-12-16 17:13:34 +0100

    finished regex for datatype matching

--- src/guess.py
+++ src/guess.py
@@ -18,14 +18,28 @@ def hasHeader(sample: str):
 def dialect(sample: str):
     return csv.Sniffer().sniff(sample)
 
+regexGeoCoord = re.compile(r"^(N|S)?0*\d{1,2}°0*\d{1,2}(′|')0*\d{1,2}\.\d*(″|\")(?(1)|(N|S)) (E|W)?0*\d{1,2}°0*\d{1,2}(′|')0*\d{1,2}\.\d*(″|\")(?(5)|(E|W))$")
 regexEMail = re.compile(r"^[a-zA-Z0-9_.-]+@[a-zA-Z0-9.-]+.[a-z.]{2,6}$")
-regexDate = re.compile(r"^[0-3]?[0-9][/.][0-3]?[0-9][/.](?:[0-9]{2})?[0-9]{2}$")
-regexTime = re.compile(r"^[0-2]\d:[0-6]\d:[0-6]\d$")
-regexNumber = re.compile(r"^[+-]?\d+([,.]\d+)?$")
+regexURL = re.compile(r"^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$")
+regexDatetime = re.compile(r"^(?:(?:31(/|-|.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(/|-|.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(/|-|.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(/|-|.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2}).([0-1]\d|2[0-3]):[0-5]\d(:[0-5]\d)*$")
+regexDate = re.compile(r"^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$")
+regexTime = re.compile(r"^([0-1]\d|2[0-3]):[0-5]\d(:[0-5]\d)*$")
+regexFloat = re.compile(r"^[+-]?\d+[,.]\d+$") 
+regexInteger = re.compile(r"^[+-]?\d+$")
+regexBoolean = re.compile(r"^(wahr|falsch|ja|nein|yes|no|true|false|t|f|1|0)$", flags=re.IGNORECASE)
 
 def dataType(sample: str):
+    if regexGeoCoord.match(sample):
+        return "GeoCoord"
+
     if regexEMail.match(sample):
         return "Email"
+
+    if regexURL.match(sample):
+        return "URL"
+
+    if regexDatetime.match(sample):
+        return "Datetime"
 
     if regexDate.match(sample):
         return "Date"
@@ -33,8 +47,14 @@ def dataType(sample: str):
     if regexTime.match(sample):
         return "Time"
 
-    if regexNumber.match(sample):
-        return "Number"
+    if regexFloat.match(sample):
+        return "Float"
+
+    if regexInteger.match(sample):
+        return "Integer"
+
+    if regexBoolean.match(sample):
+        return "Boolean"
 
     return "Text"
 
